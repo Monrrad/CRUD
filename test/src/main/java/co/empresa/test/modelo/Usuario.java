e1060945496ac1ce67d2c550eb9a6f2e2b57a3da
Monrrad <ramondavidmc@ufps.edu.co>
2023-11-13 21:05:18 -0500

    Usuario DAO

--- test/src/main/java/co/empresa/test/modelo/Usuario.java
+++ test/src/main/java/co/empresa/test/modelo/Usuario.java
@@ -2,10 +2,15 @@ package co.empresa.test.modelo;
 
 import java.io.Serializable;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Usuario implements Serializable {
 	
 	private Integer id;
